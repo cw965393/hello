37981a524ebea70035a2cf600c2641e510896e30
Cory Williams <cw965393@wtstudents.wtamu.edu>
2018-09-19 11:09:28 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2018_9_19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
